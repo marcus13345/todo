9ff2cafbb9666c6b2385a958aad058e75846e9a4
Valerie <marcus@valnet.xyz>
2021-08-27 08:21:56 -0400

    update scripto

--- start.sh
+++ start.sh
@@ -1,1 +1,13 @@
-(cd todo && git fetch && git pull && yarn && yarn compile && yarn start)
+pwd;
+read -p "continue"
+git fetch;
+read -p "continue"
+git pull;
+read -p "continue"
+yarn;
+read -p "continue"
+yarn compile;
+read -p "continue"
+yarn start;
+read -p "continue"
+
